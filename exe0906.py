9217841b1a6749d6d33aaefa6b9b26653234f54f
Junior Pedroso <sintasebemagora@gmail.com>
2021-11-12 20:21:26 -0300

    Create exe0906.py

---
+++ exe0906.py
@@ -0,0 +1,67 @@
+class Restaurante():
+    """[Minha primeira Classe em Python]
+    """
+
+    def __init__(self, nome_restaurante, tipo_cozinha):
+        """[Inicializa os atributos nome_restaurante e tipo_cozinha]
+
+        Args:
+            nome_restaurante ([type]): [description]
+            tipo_cozinha ([type]): [description]
+        """
+        self.nome_restaurante = nome_restaurante
+        self.tipo_cozinha = tipo_cozinha
+        self.number_served = 0
+
+    def descreva_restaurante(self):
+        """[Exibir informações: nome e tipo]
+        """
+        print(f'Nome do Restaurante: {self.nome_restaurante.title()}')
+        print(f'Tipo de cozinha: {self.tipo_cozinha.title()}')
+
+    def abrir_restaurante(self):
+        """[Mensagem informando que o restaurante está aberto]
+        """
+        print(f'O restaurante {self.nome_restaurante.title()}' +
+              ' está aberto!')
+        print(f'Foram servidos {self.number_served} clientes.')
+
+    def set_number_served(self, clients):
+        self.number_served = clients
+
+    def increment_number_served(self, clients):
+        self.number_served += clients
+
+
+class IceCreamStand(Restaurante):
+    """[Representa aspectos específicos de uma sorveteria]
+
+    Args:
+        Restaurante ([type]): [description]
+    """
+
+    def __init__(self, nome_restaurante, tipo_cozinha):
+        """[Inicializa os atributos da classe-pai]
+
+        Args:
+            nome_restaurante ([type]): [description]
+            tipo_cozinha ([type]): [description]
+        """
+        super().__init__(nome_restaurante, tipo_cozinha)
+        self.flavors = ['abacaxi', 'baunilha', 'chocolate', 'limão',
+                        'morango', 'napolitano', 'passas ao rum']
+
+    def show_flavors(self):
+        print('Sabores de sorvetes')
+        for flavor in self.flavors:
+            print(f'- {flavor.title()}')
+
+
+my_icecream = IceCreamStand('gelatt', 'sorveteria')
+my_icecream.show_flavors()
+print('\nForam atendidos 30 clientes')
+my_icecream.set_number_served(30)
+my_icecream.abrir_restaurante()
+print('\nForam atendidos mais 6 clientes')
+my_icecream.increment_number_served(6)
+my_icecream.abrir_restaurante()
