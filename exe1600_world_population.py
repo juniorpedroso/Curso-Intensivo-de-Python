ced7ca30c16589426a1dd1ff38433aad94ef7966
Junior Pedroso <sintasebemagora@gmail.com>
2021-11-29 16:26:22 -0300

    Exercícios

--- exe1600_world_population.py
+++ exe1600_world_population.py
@@ -1,18 +1,42 @@
 import json
+import pygal
+from pygal.style import RotateStyle
 from exe1600_country_codes import get_country_code
+
 
 # Carrega os dados em uma lista
 filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
-# Exibe a população de cada país em 2010
+# Constrói um dicionário com dados das populações
+cc_populations = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
-        country_name = pop_dict['Country Name']
+        country = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
-        code = get_country_code(country_name)
+        code = get_country_code(country)
         if code:
-            print(code + ': ' + str(population))
-        else:
-            print('ERROR - ' + country_name)
+            cc_populations[code] = population
+
+# Agrupa os países em três níveis populacionais
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+
+# Vê quantos países estão em cada nível
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
+wm_style = RotateStyle('#336699')
+wm = pygal.maps.world.World(style=wm_style)
+wm.title = 'World Population in 2010, by Country'
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
+
+wm.render_to_file('world_population_2.svg')
