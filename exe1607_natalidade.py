dd513d0c72a231e400842874aeadb3f670d72090
Junior Pedroso <sintasebemagora@gmail.com>
2021-11-30 18:00:37 -0300

    Exercícios

--- exe1607_natalidade.py
+++ exe1607_natalidade.py
@@ -1,64 +1,32 @@
 # Extraindo dados de um arquivo CSV e exibindo com Matplotlib
-
 import csv
-from datetime import datetime
 from matplotlib import pyplot as plt
+from exe1600_country_codes import get_country_code
 
 # Obtém as datas e as temperaturas máximas e mínimas do arquivo
 # filename  = 'sitka_weather_07-2014.csv'
 # filename  = 'sitka_weather_2014.csv'
 filename  = 'data/taxa_natalidade.csv'
+
+
 with open(filename) as f:
     reader = csv.reader(f)
-    # header_row = next(reader)
+    header_row = next(reader)
+    header_row = next(reader)
+    header_row = next(reader)
+    header_row = next(reader)
+    header_row = next(reader)
+    # print(header_row)
 
-    for index, column_header in enumerate(reader):
-         print(index, column_header)
-
-''' # Obtém as datas e as temperaturas máximas e mínimas do arquivo
-    dates, highs_f, lows_f = [], [], []
+     
+    dict_paises = {}
     for row in reader:
-        try:
-            # current_date = datetime.strptime(row[0], "%Y-%m-%d")
-            current_date = datetime.strptime(row[0], "%Y-%m-%d")
-            high_f = int(row[1])
-            low_f = int(row[3])
+        dict_paises['Country Name'] = row[0]
+        dict_paises['Code'] = get_country_code(row[0])
 
-        except ValueError:
-            print(current_date, 'missing data')
-        
-        else:
-            dates.append(current_date)
-            highs_f.append(high_f)
-            lows_f.append(low_f)
-            
+for country, code in dict_paises.items():
+    print(country, code)
 
+    # for produto, preco in lanchonete.items():
+    #     print(produto, preco)
 
-    # Converte as temperaturas para Celsius
-    highs_c = [int(((f - 32) / 1.8)) for f in highs_f]
-    lows_c = [int(((f - 32) / 1.8)) for f in lows_f]
-
-    
-    # Faz a plotagem dos dados em Fahrenheit
-    fig = plt.figure(dpi=128, figsize=(10,6))
-    plt.plot(dates, highs_f, c='red', alpha=0.5)
-    plt.plot(dates, lows_f, c='blue', alpha=0.5)
-
-    # Sombreando um gráfico
-    plt.fill_between(dates, highs_f, lows_f, facecolor='blue', alpha=0.1)
-
-    # Temperaturas em Celsius
-    plt.plot(dates, highs_c, c='green')
-    plt.plot(dates, lows_c, c='black')
-    # Sombreando um gráfico
-    plt.fill_between(dates, highs_c, lows_c, facecolor='yellow', alpha=0.1)
-
-    # Formata o gráfico
-    title = 'Daily high and low temperatures - 2014\nDeath Valley, CA'
-    plt.title(title, fontsize=24)
-    plt.xlabel('', fontsize=16)
-    fig.autofmt_xdate()
-    plt.ylabel('Temperature (F)', fontsize=16)
-    plt.tick_params(axis='both', which='major', labelsize=16)
-
-    plt.show()'''
---
+++ exe1700_python_repos_c.py
@@ -0,0 +1,32 @@
+# Processando uma resposta de API
+
+import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightStyle as LS
+
+# Faz uma chamada de API e armazena a resposta
+# url = 'https://api.github.com/search/repositories?\
+# q=language:python&sorts=stars'
+url = 'https://api.github.com/search/repositories?q=language:python&sorts=stars'
+r = requests.get(url)
+print('Status code:', r.status_code)
+
+# Armazena a resposta da API em uma variável
+response_dict = r.json()
+print('Total repositories:', response_dict['total_count'])
+
+# Explora informações sobre os repositórios
+repo_dicts = response_dict['items']
+
+names, stars = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Cria a visualização
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
