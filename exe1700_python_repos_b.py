c00ea0271c7f373a76aae26fbead1369f85857f7
Junior Pedroso <sintasebemagora@gmail.com>
2021-11-30 20:18:00 -0300

    Exercícios

--- exe1700_python_repos_b.py
+++ exe1700_python_repos_b.py
@@ -1,33 +1,41 @@
-# Processando uma resposta de API
-
 import requests
-import plotly.offline as py
-import plotly.graph_objs as go
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
-py.init_notebook_mode(connected=True)
-
-# Faz uma chamada de API e armazena a resposta
-url = 'https://api.github.com/search/repositories?\
-q=language:python&sorts=stars'
+url = 'https://api.github.com/search/repositories?q=language:\
+       python&sort=stars'
 r = requests.get(url)
-print('Status code:', r.status_code)
+print("Status code:", r.status_code)
 
-# Armazena a resposta da API em uma variável
 response_dict = r.json()
-print('Total repositories:', response_dict['total_count'])
+print("Total repositories:", response_dict['total_count'])
 
-# Explora informações sobre os repositórios
 repo_dicts = response_dict['items']
+names, plot_dicts = [], []
 
-names, stars = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+    }
+    plot_dicts.append(plot_dict)
 
-trace = go.Scatter(x = names, 
-                   y = stars,
-                   mode='bars')
 
-data = [trace]
+my_style = LS('#333366', base_style=LCS)
 
-py.iplot(data)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos_b.svg')
